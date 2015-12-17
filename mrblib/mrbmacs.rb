bf67b44d47b72d4789f95b0373ba53846486ceb9
masahino <masahino@gmail.com>
2015-12-17 22:39:15 +0900

    move some methods to mrbmacs-base

--- mrblib/mrbmacs.rb
+++ mrblib/mrbmacs.rb
@@ -2,51 +2,6 @@ module Mrbmacs
   class Application
     include Scintilla
 
-    attr_accessor :frame, :mark_pos
-    attr_accessor :current_buffer, :buffer_list, :prev_buffer
-    attr_accessor :mode
-    attr_accessor :theme
-    attr_accessor :file_encodings
-    def initialize(init_filename, opts = nil)
-      @frame = Mrbmacs::Frame.new()  
-      @keymap = ViewKeyMap.new(@frame.view_win)
-      @command_list = @keymap.command_list
-      @echo_keymap = EchoWinKeyMap.new(@frame.echo_win)
-      @style_list = {}
-      @mode = Mode.new
-      @theme = SolarizedDarkTheme.new
-      @mark_pos = nil
-      @buffer_list = []
-      @current_buffer = nil
-      @filename = nil
-
-      @file_encodings = []
-      load_init_file(init_filename)
-    end
-
-    def load_init_file(init_filename)
-      begin
-        File.open(init_filename, "r") do |f|
-          init_str = f.read()
-          instance_eval(init_str)
-        end
-      rescue
-        $stderr.puts $!
-      end
-    end
-
-    def extend(command)
-      if command.class.to_s == "Fixnum"
-        @frame.view_win.send_message(command)
-      else
-        begin
-          instance_eval("Mrbmacs::#{command.gsub("-", "_")}(self)")
-        rescue
-          $stderr.puts $!
-        end
-      end
-    end
-
     def doscan(prefix)
       ret, key = @frame.tk.waitkey
       key_str = prefix + @frame.tk.strfkey(key, TermKey::FORMAT_ALTISMETA)
@@ -61,18 +16,6 @@ module Mrbmacs
         end
       end
       [key, nil]
-    end
-
-    def doin()
-      key, command = doscan("")
-      if $DEBUG
-        $stderr.puts command
-      end
-      if command == nil
-        @frame.send_key(key)
-      else
-        extend(command)
-      end
     end
 
     def run(file = nil)
