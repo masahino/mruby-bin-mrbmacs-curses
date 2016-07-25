5c37aece229488df56b5d4ebe7264e7a2da088c4
masahino <masahino@gmail.com>
2016-07-25 23:09:01 +0900

    use PDcurses on mingw32

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,29 @@
 FROM hone/mruby-cli
 RUN apt-get update && apt-get install -y unzip libncurses5-dev wget
-RUN cd /tmp && wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.0.tar.gz \
-  && tar zxf ncurses-6.0.tar.gz && cd ncurses-6.0 \
-  && ./configure --prefix=/usr/i686-w64-mingw32/ --host=i686-w64-mingw32 --with-shared --enable-term-driver \
-  && make install
+#RUN cd /tmp && wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.0.tar.gz \
+#  && tar zxf ncurses-6.0.tar.gz && cd ncurses-6.0 \
+#  && ./configure --prefix=/usr/i686-w64-mingw32/ --host=i686-w64-mingw32 --without-ada --enable-warnings \
+#  --enable-asertions --disable-home-terminfo --enable-database --enable-term-driver --enable-sp-funcs \
+#  --enable-interop --disable-termcap --with-progs --enable-pc-files --enable-widec \
+#  && make install
+#RUN cd /usr/i686-w64-mingw32 && wget http://invisible-island.net/datafiles/release/mingw32.zip \
+#  && unzip mingw32.zip
 RUN cd /tmp && wget https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.14.tar.gz \
   && tar zxf libiconv-1.14.tar.gz && cd libiconv-1.14 \
-  && ./configure --prefix=/usr/i686-w64-mingw32/ --host=i686-w64-mingw32 \
+  && ./configure --prefix=/usr/i686-w64-mingw32/ --host=i686-w64-mingw32 --enable-static \
   && make install
+RUN cd /tmp && wget https://sourceforge.net/projects/pdcurses/files/pdcurses/3.4/PDCurses-3.4.tar.gz \
+  && tar zxf PDCurses-3.4.tar.gz && cd PDCurses-3.4/win32 \
+  && make -f mingwin32.mak CC=i686-w64-mingw32-gcc LINK=i686-w64-mingw32-gcc LIBCURSES=libcurses.a \
+  && cp ../curses.h /usr/i686-w64-mingw32/include \
+  && cp ../term.h /usr/i686-w64-mingw32/include \
+  && cp libcurses.a /usr/i686-w64-mingw32/lib
+#RUN cd /tmp && wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.0.tar.gz \
+#  && tar zxf ncurses-6.0.tar.gz && cd ncurses-6.0 \
+#  && ./configure --prefix=/usr/x86_64-w64-mingw32/ --host=x86_64-w64-mingw32 --enable-term-driver --enable-sp-funcs \
+#  && make install
+#RUN cd /tmp && wget https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.14.tar.gz \
+#  && tar zxf libiconv-1.14.tar.gz && cd libiconv-1.14 \
+#  && make clean \
+#  && ./configure --prefix=/usr/x86_64-w64-mingw32/ --host=x86_64-w64-mingw32 --enable-static \
+# && make install
--- build_config.rb
+++ build_config.rb
@@ -2,7 +2,7 @@ def gem_config(conf)
   conf.gembox 'default'
   conf.gem "#{MRUBY_ROOT}/mrbgems/mruby-eval"
   conf.gem "#{MRUBY_ROOT}/mrbgems/mruby-exit"
-  conf.gem :github => 'mattn/mruby-iconv' do |g|
+  conf.gem :github => 'masahino/mruby-iconv', :branch => 'add_iconvlist' do |g|
     g.linker.libraries.delete 'iconv'
   end
 #  conf.gem :github => 'gromnitsky/mruby-dir-glob'
@@ -31,6 +31,38 @@ MRuby::Build.new do |conf|
   conf.gem :github => 'mattn/mruby-require'
 end
 
+#MRuby::CrossBuild.new('x86_64-pc-linux-gnu') do |conf|
+#  toolchain :gcc
+#
+#  conf.enable_debug
+#  conf.enable_cxx_abi
+#
+#  conf.enable_bintest
+#  conf.enable_test
+#
+#  gem_config(conf)
+#  conf.gem :github => 'iij/mruby-regexp-pcre'
+##  conf.gem :github => 'mattn/mruby-require'
+#end
+
+#MRuby::CrossBuild.new('i386-pc-linux-gnu') do |conf|
+#  toolchain :gcc
+#
+#  [conf.cc, conf.cxx, conf.linker].each do |cc|
+#    cc.flags << "-m32"
+#  end
+#
+#  conf.enable_debug
+#  conf.enable_cxx_abi
+#
+#  conf.enable_bintest
+#  conf.enable_test
+#
+#  gem_config(conf)
+#  conf.gem :github => 'iij/mruby-regexp-pcre'
+##  conf.gem :github => 'mattn/mruby-require'
+#end
+
 MRuby::CrossBuild.new('x86_64-apple-darwin14') do |conf|
   toolchain :clang
 
@@ -46,10 +78,33 @@ MRuby::CrossBuild.new('x86_64-apple-darwin14') do |conf|
 
   conf.linker.libraries << 'iconv'
   conf.linker.libraries << 'stdc++'
+  conf.gem :github => 'masahino/mruby-iconv', :branch => 'add_iconvlist' do |g|
+    g.cc.flags << '-DHAVE_ICONVLIST'
+  end
   gem_config(conf)
   conf.gem :github => 'iij/mruby-regexp-pcre'
   conf.gem :github => 'mattn/mruby-require'
 end
+
+#MRuby::CrossBuild.new('i386-apple-darwin14') do |conf|
+#  toolchain :clang
+#
+#  conf.enable_cxx_abi
+#  [conf.cc, conf.linker].each do |cc|
+#    cc.command = 'i386-apple-darwin14-clang'
+#  end
+#  conf.cxx.command      = 'i386-apple-darwin14-clang++'
+#  conf.archiver.command = 'i386-apple-darwin14-ar'
+#
+#  conf.build_target     = 'i386-pc-linux-gnu'
+#  conf.host_target      = 'i386-apple-darwin14'
+#
+#  conf.linker.libraries << 'iconv'
+#  conf.linker.libraries << 'stdc++'
+#  gem_config(conf)
+#  conf.gem :github => 'iij/mruby-regexp-pcre'
+#  conf.gem :github => 'mattn/mruby-require'
+#end
 
 MRuby::CrossBuild.new('i686-w64-mingw32') do |conf|
   toolchain :gcc
@@ -64,11 +119,38 @@ MRuby::CrossBuild.new('i686-w64-mingw32') do |conf|
   conf.build_target     = 'i686-pc-linux-gnu'
   conf.host_target      = 'i686-w64-mingw32'
 
-  gem_config(conf)
+  conf.gem :github => 'masahino/mruby-iconv', :branch => 'add_iconvlist' do |g|
+    g.cc.flags << '-DHAVE_ICONVLIST'
+  end
   conf.gem :github => 'iij/mruby-regexp-pcre' do |g|
     g.cc.flags << '-DPCRE_STATIC'
   end
+  gem_config(conf)
   conf.cc.include_paths << '/usr/i686-w64-mingw32/include/ncurses'
+  conf.linker.flags << '-static'
   conf.linker.libraries << 'iconv'
   conf.linker.libraries << 'stdc++'
 end
+
+#MRuby::CrossBuild.new('x86_64-w64-mingw32') do |conf|
+#  toolchain :gcc
+#
+#  [conf.cc, conf.linker].each do |cc|
+#    cc.command = 'x86_64-w64-mingw32-gcc'
+#  end
+#  conf.cxx.command      = 'x86_64-w64-mingw32-g++'
+#  conf.archiver.command = 'x86_64-w64-mingw32-gcc-ar'
+#  conf.exts.executable  = ".exe"
+#
+#  conf.build_target     = 'x86_64-pc-linux-gnu'
+#  conf.host_target      = 'x86_64-w64-mingw32'
+#
+#  gem_config(conf)
+#  conf.gem :github => 'iij/mruby-regexp-pcre' do |g|
+#    g.cc.flags << '-DPCRE_STATIC'
+#  end
+#  conf.cc.include_paths << '/usr/x86_64-w64-mingw32/include/ncurses'
+#  conf.linker.flags << '-static'
+#  conf.linker.libraries << 'iconv'
+#  conf.linker.libraries << 'stdc++'
+#end
