c9542143caf1d7ef4eec4862d136462a2ecdb87a
masahino <masahino@gmail.com>
2019-06-07 22:27:05 +0900

    bug fix

--- mrblib/mrbmacs.rb
+++ mrblib/mrbmacs.rb
@@ -24,30 +24,21 @@ module Mrbmacs
     end
 
     def keyin()
-      doin()
-      if @frame.tk.buffer_remaining == @frame.tk.buffer_size
-        current_pos = @frame.view_win.sci_get_current_pos
-        pos1 = @frame.view_win.sci_bracematch(current_pos, 0)
-        if pos1 != -1
-          @frame.view_win.sci_brace_highlight(pos1, current_pos)
-        end
-        pos1 = @frame.view_win.sci_bracematch(current_pos - 1, 0)
-        if pos1 != -1
-          @frame.view_win.sci_brace_highlight(pos1, current_pos - 1)
-        end
-        if @mark_pos != nil
-          @frame.view_win.sci_set_anchor(@mark_pos)
-        end
-        # autocomp
-        if @frame.view_win.sci_autoc_active == 0 and @frame.char_added == true
-#          if @lsp[@current_buffer.mode.name] != nil
-#            len, candidates = @current_buffer.mode.get_completion_list(@frame.view_win)
-#            if len > 0 and candidates.length > 0
-#              @frame.view_win.sci_autoc_show(len, candidates)
-#            end
-#            @frame.char_added = false
-#          end
-        end
+      loop do
+        doin()
+        break if @frame.tk.buffer_remaining == @frame.tk.buffer_size
+      end
+      current_pos = @frame.view_win.sci_get_current_pos
+      pos1 = @frame.view_win.sci_bracematch(current_pos, 0)
+      if pos1 != -1
+        @frame.view_win.sci_brace_highlight(pos1, current_pos)
+      end
+      pos1 = @frame.view_win.sci_bracematch(current_pos - 1, 0)
+      if pos1 != -1
+        @frame.view_win.sci_brace_highlight(pos1, current_pos - 1)
+      end
+      if @mark_pos != nil
+        @frame.view_win.sci_set_anchor(@mark_pos)
       end
     end
 
@@ -55,11 +46,6 @@ module Mrbmacs
       add_io_read_event(STDIN) { |app, io| keyin }
       @frame.view_win.refresh
       loop do
-        current_pos = @frame.view_win.sci_get_current_pos
-        x = @frame.view_win.sci_point_x_from_position(0, current_pos)
-        y = @frame.view_win.sci_point_y_from_position(0, current_pos)
-        @frame.view_win.setpos(y, x)
-        @frame.view_win.sci_set_empty_selection(current_pos)
         while @frame.sci_notifications.length > 0
           e = @frame.sci_notifications.shift
           if @sci_handler[e['code']] != nil
@@ -67,6 +53,12 @@ module Mrbmacs
           end
         end
         @frame.view_win.refresh
+        # set cursor pos for IME
+        current_pos = @frame.view_win.sci_get_current_pos
+        x = @frame.view_win.sci_point_x_from_position(0, current_pos)
+        y = @frame.view_win.sci_point_y_from_position(0, current_pos)
+        @frame.view_win.setpos(y, x)
+        @frame.view_win.sci_set_empty_selection(current_pos)
 
         readable, writable = IO.select(@readings)
         readable.each do |ri|
