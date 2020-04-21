af4c4adcb77f21f9c6931e980ebe97b08cd35ded
masahino <masahino@gmail.com>
2020-04-22 08:24:50 +0900

    add test

--- bintest/mrbmacs-curses.rb
+++ bintest/mrbmacs-curses.rb
@@ -1,8 +1,15 @@
 require 'open3'
 $script_dir = File.dirname(__FILE__) + "/scripts/"
+
 assert('init buffer') do
   stdout, stderr, status = Open3.capture3("#{cmd('mrbmacs-curses')} -l #{$script_dir}init_buffer")
   assert_equal 0, status.to_i
   lines = stderr.split("\n")
   assert_equal "*scratch*", lines[0]
+end
+
+assert('split window') do
+  stdout, stderr, status = Open3.capture3("#{cmd('mrbmacs-curses')} -l #{$script_dir}split_window")
+  assert_equal 0, status.to_i
+  assert_equal 100, stderr.length
 end
