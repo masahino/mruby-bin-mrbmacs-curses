ac56cda662cc296c27f9fe1148d998b8c7a6c214
masahino <masahino@gmail.com>
2020-07-23 20:47:16 +0900

    del set_anchor

--- mrblib/mrbmacs-curses.rb
+++ mrblib/mrbmacs-curses.rb
@@ -43,9 +43,6 @@ module Mrbmacs
       if pos1 != -1
         @frame.view_win.sci_brace_highlight(pos1, current_pos - 1)
       end
-      if @mark_pos != nil
-        @frame.view_win.sci_set_anchor(@mark_pos)
-      end
     end
 
     def editloop()
