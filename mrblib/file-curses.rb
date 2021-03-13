e656dc1339cacb9cc0f497cc6438f5cd1838834e
masahino <masahino@gmail.com>
2021-03-14 08:45:24 +0900

    read directory name

--- mrblib/file-curses.rb
+++ mrblib/file-curses.rb
@@ -1,5 +1,32 @@
 module Mrbmacs
   class ApplicationCurses < Application
+    def read_dir_name(prompt, default_directory = nil)
+      prefix_text = default_directory
+      if prefix_text[-1] != '/'
+        prefix_text += '/'
+      end
+      dirname = @frame.echo_gets(prompt, prefix_text) do |input_text|
+        dir_list = []
+        len = 0
+        if input_text[-1] == '/'
+          dir_list = Dir.entries(input_text).select {|f| File.directory?(f)}
+        else
+          dir = File.dirname(input_text)
+          fname = File.basename(input_text)
+          qfname = Regexp.quote(fname)
+          Dir.foreach(dir) do |item|
+            if File.directory?(item) and item =~ /^#{qfname}/
+              dir_list.push(item)
+            end
+          end
+          len = fname.length
+        end
+        [dir_list.sort.join(' '), len]
+      end
+      @frame.modeline_refresh(self)
+      return dirname
+    end
+
     def read_file_name(prompt, directory, default_name = nil)
       prefix_text = directory + "/"
       if default_name != nil
