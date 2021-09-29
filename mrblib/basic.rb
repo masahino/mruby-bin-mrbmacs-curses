1c2f45794e4cd36d00ac28cb0e7b22a2fbe9f18b
masahino <masahino@gmail.com>
2021-09-29 20:17:47 +0900

    cosmetic change

--- mrblib/basic.rb
+++ mrblib/basic.rb
@@ -1,21 +1,22 @@
 module Mrbmacs
+  # ApplicationCurses
   class ApplicationCurses < Application
-    def copy_region()
+    def copy_region
       super
       str = @frame.view_win.get_clipboard
       if Scintilla::PLATFORM == :CURSES_WIN32
-        IO.popen('clip.exe', 'r+') {|f| f << str}
+        IO.popen('clip.exe', 'r+') { |f| f << str }
       else
         # try pbcopy
         ret = `type pbcopy 2>/dev/null`
         if $?.exitstatus == 0
-          IO.popen('pbcopy', 'w') {|f| f << str}
+          IO.popen('pbcopy', 'w') { |f| f << str }
         end
       end
     end
 
-    def yank()
-      if @frame.view_win.get_clipboard == ""
+    def yank
+      if @frame.view_win.get_clipboard == ''
         ret = `type pbpaste 2>/dev/null`
         if $?.exitstatus == 0
           clipboard_text = `pbpaste`
@@ -25,11 +26,11 @@ module Mrbmacs
       @frame.view_win.sci_paste
     end
 
-    def isearch_forward()
+    def isearch_forward
       isearch("I-search: ", false)#, @frame.view_win.sci_get_length)
     end
 
-    def isearch_backward()
+    def isearch_backward
       isearch("I-search backward: ", true) #0)
     end
 
@@ -45,12 +46,12 @@ module Mrbmacs
       echo_win.refresh
       view_win.sci_set_target_start(start_pos)
       view_win.sci_set_target_end(end_pos)
-      search_text = ""
+      search_text = ''
       while true
         ret, key = @frame.waitkey(echo_win)
         key_str = @frame.strfkey(key)
-        if key_str == "C-s"
-          if search_text != ""
+        if key_str == 'C-s'
+          if search_text != ''
             backward = false
             view_win.sci_set_target_start(view_win.sci_get_current_pos)
             end_pos = view_win.sci_get_length
@@ -61,10 +62,10 @@ module Mrbmacs
             @frame.echo_win.refresh
             next
           end
-        elsif key_str == "C-r"
-          if search_text != ""
+        elsif key_str == 'C-r'
+          if search_text != ''
             backward = true
-            start_pos = view_win.sci_get_current_pos-search_text.length
+            start_pos = view_win.sci_get_current_pos - search_text.length
             view_win.sci_set_target_start(start_pos)
             end_pos = 0
             view_win.sci_set_target_end(end_pos)
@@ -74,16 +75,16 @@ module Mrbmacs
             @frame.echo_win.sci_add_text(search_text.bytesize, search_text)
             @frame.echo_win.refresh
           end
-        elsif key_str == "C-g"
+        elsif key_str == 'C-g'
           view_win.sci_goto_pos(orig_pos)
           break
-        elsif key_str =~ /^[CM]-/
-    #    elsif (key.modifiers & TermKey::KEYMOD_CTRL > 0) or (key.modifiers & TermKey::KEYMOD_ALT > 0)
+        elsif key_str =~ /^[CM]-/ || key_str == 'Enter'
+          #    elsif (key.modifiers & TermKey::KEYMOD_CTRL > 0) or (key.modifiers & TermKey::KEYMOD_ALT > 0)
           break
         else
           @frame.send_key(key, echo_win)
           echo_win.refresh
-          search_text = @frame.echo_win.sci_get_text(@frame.echo_win.sci_get_length+1)
+          search_text = @frame.echo_win.sci_get_text(@frame.echo_win.sci_get_length + 1)
           @last_search_text = search_text
         end
         ret = view_win.sci_search_in_target(search_text.bytesize, search_text)
