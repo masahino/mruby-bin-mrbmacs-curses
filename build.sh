2691e5ca0f71f2f81a75add95403eefe16dd143c
masahino <masahino@gmail.com>
2022-06-09 07:54:44 +0900

    build with mruby 3.1.0

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export MRUBY_CONFIG=`pwd`/.travis_config.rb
 if [ -z "${MRUBY_VERSION}" ] ; then
-  export MRUBY_VERSION="3.0.0"
+  export MRUBY_VERSION="3.1.0"
 fi
 
 if [ ! -d "./mruby/src" ]; then
