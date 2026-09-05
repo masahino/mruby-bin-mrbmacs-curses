b28ef03f04d8baed54d2a9d38379eafb154c7b01
masahino <masahino@gmail.com>
2026-09-05 13:49:57 +0900

    echo_set_prompt

--- mrblib/frame_curses.rb
+++ mrblib/frame_curses.rb
@@ -252,12 +252,6 @@ module Mrbmacs
       input_text
     end
 
-    def echo_set_prompt(prompt)
-      @echo_win.sci_set_margin_widthn(3, @echo_win.sci_text_width(Scintilla::STYLE_DEFAULT, prompt))
-      @echo_win.sci_margin_set_text(0, prompt)
-      @echo_win.refresh
-    end
-
     def read_buffername(prompt)
       echo_gets(prompt)
     end
