7bf76a9d7c1cd748c0ba704a59fbb35ca4287d84
masahino <masahino@gmail.com>
2026-09-06 11:53:04 +0900

    streamline build_config

--- build_config.rb
+++ build_config.rb
@@ -8,37 +8,46 @@ end
 
 MRuby::Build.new do |conf|
   if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
-    toolchain :visualcpp
+    conf.toolchain :visualcpp
   else
-    toolchain :gcc
+    conf.toolchain :gcc
   end
 
-  enable_debug
+  conf.enable_debug
 
-  conf.cc.defines = %w[MRB_ENABLE_ALL_SYMBOLS]
-  conf.cc.defines << %w[MRB_UTF8_STRING]
+  # UTF-8-aware String (String#length / [] / index operate on characters).
+  conf.cc.defines += %w[MRB_UTF8_STRING]
 
   conf.gembox 'default'
+
+  conf.gem github: 'masahino/mruby-mrbmacs-lsp'
+  conf.gem github: 'masahino/mruby-lsp-client' do |g|
+    g.skip_test = true
+  end
+  conf.gem github: 'masahino/mruby-mrbmacs-dap'
+  conf.gem github: 'masahino/mruby-mrbmacs-aichat'
+  conf.gem github: 'masahino/mruby-mrbmacs-agent'
+  conf.gem github: 'masahino/mruby-debug'
+  conf.gem github: 'masahino/mruby-mrbmacs-themes-base16'
+  conf.gem github: 'masahino/mruby-mrbmacs-themes-tomorrow'
 
   conf.gem github: 'mattn/mruby-iconv' do |g|
     g.linker.libraries.delete 'iconv' if RUBY_PLATFORM.include?('linux')
     g.skip_test = true
   end
+  conf.gem github: 'iij/mruby-regexp-pcre' do |g|
+    g.skip_test = true
+  end
+
   if RUBY_PLATFORM.downcase =~ /msys|mingw/
-    conf.cc.include_paths << "#{MRUBY_ROOT}/../misc"
+    conf.cc.include_paths << File.expand_path('misc', __dir__)
     conf.cc.include_paths << "#{ENV['MINGW_PREFIX']}/include/pdcurses"
     conf.cc.flags << '-DPDC_WIDE'
     conf.cc.flags << '-DPDC_FORCE_UTF8'
   end
-  conf.gem github: 'iij/mruby-regexp-pcre' do |g|
-    g.skip_test = true
-  end
-  conf.gem github: 'masahino/mruby-mrbmacs-lsp'
-  conf.gem github: 'masahino/mruby-lsp-client' do |g|
-    g.skip_test = true
-  end
 
-  conf.gem "#{MRUBY_ROOT}/.."
+  conf.gem File.expand_path(__dir__)
+
   conf.linker.libraries << 'stdc++'
   conf.linker.libraries << 'pthread'
   if RUBY_PLATFORM.downcase =~ /msys|mingw/
@@ -47,7 +56,6 @@ MRuby::Build.new do |conf|
     conf.linker.libraries.delete 'ncurses'
   end
 
-  # bintest
   conf.enable_bintest
   conf.enable_test
 end
